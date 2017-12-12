f8cc52943c131f8253f3a4a56564f861dcbdfe9c
Tigran Sedrakyan <tikona.sedrakyan@gmail.com>
2017-12-13 00:00:54 +0400

    added 'shutdown' and 'who are you' commands, minor fixes

--- bot.js
+++ bot.js
@@ -85,12 +85,12 @@ MongoClient.connect(url, function(err, db) {
 });
 
 
-
-
-
-/*controller.hears(['what\'s up', 'wazzup'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.reply(message, 'Nth much, waddup with you, bruh? Here\'s some info from ur dashboard: \n (info)');
-})*/
+// For future use
+// TODO: Add slash commands and user tag recognition
+// TODO: Add 'wazzup' command
+// TODO: Add 'help' command
+// TODO: Add 'list courses' command
+// TODO: Add notification on new event (grade, assignment, exam, etc.)
 
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
     var currentUser;
@@ -427,7 +427,6 @@ controller.hears(['get grade for assignment (.*) of course (.*)'], 'direct_messa
     })
 })
 
-/*
 controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     var name = message.match[1];
     controller.storage.users.get(message.user, function(err, user) {
@@ -443,112 +442,53 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_men
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
 
 
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection('users').findOne({ slack_id: message.user }, function(err, user) {
+            if (user.type === "admin") {
+                bot.startConversation(message, function(err, convo) {
+                    convo.ask('Are you sure you want me to shutdown?', [{
+                            pattern: bot.utterances.yes,
+                            callback: function(response, convo) {
+                                convo.say('Bye!');
+                                convo.next();
+                                setTimeout(function() {
+                                    process.exit();
+                                }, 3000);
+                            }
+                        },
+                        {
+                            pattern: bot.utterances.no,
+                            default: true,
+                            callback: function(response, convo) {
+                                convo.say('*Phew!*');
+                                convo.next();
+                            }
+                        }
+                    ])
+                })
+            } else {
+                bot.reply(message, "Sorry, only my lord can shut me down :(")
             }
-        }
-        ]);
-    });
-});
+        })
+    })
+})
 
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['who are you', 'what is your name'],
+    'direct_message,direct_mention,mention',
+    function(bot, message) {
 
         var hostname = os.hostname();
         var uptime = formatUptime(process.uptime());
 
         bot.reply(message,
             ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+            '>. I will be helping you with your studies.');
 
     });
 
@@ -569,4 +509,3 @@ function formatUptime(uptime) {
     uptime = uptime + ' ' + unit;
     return uptime;
 }
-*/
